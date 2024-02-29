71928c290c14de113611be70a6cd73894eb03e53
hashi-kazuya <ka-hashi@hs21.co.jp>
2024-02-29 10:18:30 +0900

    2836 issue2807でait-templateを修正したので既存のAITに反映する

--- deploy/container/repository/my_ait.py
+++ deploy/container/repository/my_ait.py
@@ -642,7 +642,7 @@ def move_log(file_path: str=None) -> str:
 #########################################
 
 @log(logger)
-@ait_main(ait_output, path_helper)
+@ait_main(ait_output, path_helper, is_ait_launch)
 def main() -> None:
 
     # load inventory
